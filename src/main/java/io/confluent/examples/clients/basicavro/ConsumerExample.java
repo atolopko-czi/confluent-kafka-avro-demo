19b5e4e6a774c1f7d276a9d824de7c667925b91a
Andrew Tolopko <atolopko@chanzuckerberg.com>
2019-10-04 09:24:03 -0400

    Consumer group for schema-v2

--- src/main/java/io/confluent/examples/clients/basicavro/ConsumerExample.java
+++ src/main/java/io/confluent/examples/clients/basicavro/ConsumerExample.java
@@ -22,7 +22,7 @@ public class ConsumerExample {
     public static void main(final String[] args) {
         final Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test-payments");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "transactions-v2");
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
         props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
